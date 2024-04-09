d30b7485c059781491fcf1265ff8f9c9949f0645
De oliveira Hugo <hugdeoliveira1@gmail.com>
2024-04-09 14:28:12 +0200

    Création de la classe JeuxOlympiques avec les méthodes non remplies.

---
+++ JeuxOlympiques.java
@@ -0,0 +1,32 @@
+public class JeuxOlympiques{
+
+    private int annee;
+
+    public JeuxOlympiques(int annee){
+        this.annee = annee;
+    }
+
+    public epreuves getEpreuves(){
+        
+    }
+
+    public void addEpreuve(Epreuve epv){
+        epreuves.add(epv);
+    }
+
+    public void participerAthlete(Athlete ath, Epreuve epv){
+
+    }
+
+    public void participerEquipe(Equipe equ){
+
+    }
+
+    public int getScoreAthlete(Athlete ath){
+
+    }
+
+    public int getScoreEquipe(Equipe equ){
+        
+    }
+}
