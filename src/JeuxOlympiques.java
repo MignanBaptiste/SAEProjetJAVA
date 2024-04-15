7ffb396e60d003bdc36297af7e9dcf77e89e53ca
De oliveira Hugo <hugdeoliveira1@gmail.com>
2024-04-15 11:49:56 +0200

    Ajout des commentaires et des setters

--- src/JeuxOlympiques.java
+++ src/JeuxOlympiques.java
@@ -6,28 +6,43 @@ public class JeuxOlympiques{
         this.annee = annee;
     }
 
-    
+    /* Retourne les épreuves dans la liste d'épreuves */
     public epreuves getEpreuves(){
         
     }
 
+    /* Ajoute une épreuve dans la la liste d'épreuves */
     public void addEpreuve(Epreuve epv){
         epreuves.add(epv);
     }
 
+    /* Fait participer un athlète à une épreuve donnée */
     public void participerAthlete(Athlete ath, Epreuve epv){
 
     }
 
-    public void participerEquipe(Equipe equ){
+    /*  Fait participer une équipe à une épreuve donnée*/
+    public void participerEquipe(Equipe equ, Epreuve epv){
 
     }
 
+    /* Renvoy le score d'un athlète */
     public int getScoreAthlete(Athlete ath){
 
     }
 
+    /* Renvoy le score d'un équipe */
     public int getScoreEquipe(Equipe equ){
         
+    }
+
+    /* Modifie le score d'une équipe */
+    public int setScoreEquipe(Equipe equ){
+
+    }
+
+    /* Modifie le score d'un athlète */
+    public int setScoreAthlète(Athlete ath){
+
     }
 }
