9468d98bc924408a02f07d1ce5c10fc56050a1c5
valentin-hun <valentin.hun@etu.univ-orleans.fr>
2024-04-16 15:27:03 +0200

    Création du squelette de la classe Equipe

---
+++ src/Equipe.java
@@ -0,0 +1,57 @@
+import java.util.List;
+
+public class Equipe implements Participant{
+    private String nomRquipe;
+    private int score;
+    private Pays pays;
+    private List<Athlete> athletes;
+
+    /**
+     * Création d'une nouvelle équipe
+     * possédant un nom, un score, un pays, et une liste d'athlètes.
+     * @param ath
+     */
+    public Equipe(String nomRquipe) {
+
+    }
+
+    /**
+     * Renvoie le nom de l'équipe
+     * @return
+     */
+    public String getNomRquipe() {
+
+    }
+
+    /**
+     * Renvoie le score de l'équipe
+     * @return
+     */
+    public int getScore() {
+
+    }
+
+    /**
+     * Renoive le pays auquel appartient l'équipe
+     * @return
+     */
+    public Pays getPays() {
+
+    }
+    
+    /**
+     * Renvoie la liste des athlètes de l'équipe
+     * @return
+     */
+    public List<Athlete> getAthletes() {
+
+    }
+
+    /**
+     * Ajoute un athlète à l'équipe
+     * @param ath
+     */
+    public void addAthlete(Athlete ath) {
+
+    }
+}
