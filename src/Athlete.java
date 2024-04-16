65af0f3d6edd6176e9f8767774dabd5d8a7b603c
valentin-hun <valentin.hun@etu.univ-orleans.fr>
2024-04-16 14:38:18 +0200

    Création du squelette de la classe Athlète + javadoc

---
+++ src/Athlete.java
@@ -0,0 +1,88 @@
+public class Athlete {
+    private String nomAthlete;
+    private String prenomAthlete;
+    private String sexe;
+    private int force;
+    private int agilite;
+    private int endurance;
+    private int score;
+    private Pays pays;
+
+    /**
+     * Créer un nouvel athlète
+     * @param nom
+     * @param prenom
+     * @param sexe
+     * @param force
+     * @param agilite
+     * @param endurance
+     * @param score
+     */
+    public Athlete(String nom, String prenom, String sexe, int force, int agilite, int endurance, int score) {
+
+    }
+
+    /**
+     * Revoie le nom de l'athlète
+     * @return
+     */
+    public String getNomAthlete() {
+
+    }
+
+    /**
+     * Renvoie le prenom de l'athlète
+     * @return
+     */
+    public String getPrenomAthlete() {
+
+    }
+
+    /**
+     * Renvoie le sexe de l'athlète
+     * @return
+     */
+    public String getSexe() {
+
+    }
+
+    /**
+     * Renvoie la force de l'athlète
+     * @return
+     */
+    public int getForce() {
+
+    }
+
+    /**
+     * Renvoie l'agilité de l'athlète
+     * @return
+     */
+    public int getAgilite() {
+
+    }
+
+    /**
+     * Renvoie l'endurance de l'athlète
+     * @return
+     */
+    public int getEndurance() {
+    
+    }
+
+    /**
+     * Renvoie le score de l'athlète
+     * @return
+     */
+    public int getScore() {
+
+    }
+
+    /**
+     * Renovie le pays auquel appartient l'athlète
+     * @return
+     */
+    public Pays getPays() {
+
+    }
+}
