f0d18abcc880e5fc60ea073e435fcdcb9995c503
MignanBaptiste <baptiste.mignan@etu.univ-orleans.fr>
2024-04-16 15:28:34 +0200

    Ajout de la documentation pour Epreuve et ses héritiés

--- src/Collective.java
+++ src/Collective.java
@@ -1,3 +1,9 @@
+/** Class d'épreuve collective, hérite de la classe Epreuve */
 public class Collective extends Epreuve{
+    /**
+     * Permet de créer une épreuve collective
+     * @param sexe
+     * @param sport
+     */
     public Collective(String sexe, Sport sport){super(sexe, sport);}
 }
--- src/Epreuve.java
+++ src/Epreuve.java
@@ -1,14 +1,43 @@
 import java.util.List;
-
+/** Class permettant de créer une épreuve ayant des participants */
 public abstract class Epreuve {
     private String sexe;
     private Sport sport;
     private List<Participant> participants;
+    /**
+     * Permet de créer une épreuve
+     * @param sexe
+     * @param sport
+     */
     public Epreuve(String sexe, Sport sport){}
+    /**
+     * Renvoie le sexe de l'épreuve
+     * @return String
+     */
     public String getSexe(){return "";}
+    /**
+     * Renvoie le sport de l'épreuve
+     * @return Sport
+     */
     public Sport getSport(){return null;}
+    /**
+     * Permet d'ajouter un participant à une épreuve
+     * @param participant
+     */
     public void addParticipant(Participant participant){};
+    /**
+     * Renvoie la liste des participants à l'épreuve
+     * @return List<Participant>
+     */
     public List<Participant> getParticipants(){return null;}
+    /**
+     * Renvoie le gagnant de l'épreuve
+     * @return Participant
+     */
     public Participant gagnant(){return null;}
+    /**
+     * Renvoie la liste des participants triée selon leur classement
+     * @return
+     */
     public List<Participant> classement(){return null;}
 }
