3d0616f066b11fda461ca8f397faeda36c7ba4b5
valentin-hun <valentin.hun@etu.univ-orleans.fr>
2024-05-17 10:17:39 +0200

    ajout des commentaires des tests de equipe

--- tests/TestEquipe.java
+++ tests/TestEquipe.java
@@ -1,3 +1,4 @@
+package tests;
 import jo.*;
 import org.junit.*;
 import static org.junit.Assert.assertEquals;
@@ -6,12 +7,14 @@ import static org.junit.Assert.fail;
 import java.util.Arrays;
 
 public class TestEquipe {
+    /** On veut pouvoir créer une nouvelle équipe  */
     @Test
     public void testEquipe(){
         Pays france = new Pays("France");
         Equipe equ1 = new Equipe("equ1", france);
     }
 
+    /** On veut obtenir le nom de l'équipe */
     @Test
     public void testGetNomEquipe(){
         Pays france = new Pays("France");
@@ -19,6 +22,7 @@ public class TestEquipe {
         assertEquals("equ1", equ1.getNomEquipe());
     }
 
+    /** On veut obtenir le score de l'équipe */
     @Test
     public void testGetScore(){
         Pays france = new Pays("France");
@@ -29,6 +33,7 @@ public class TestEquipe {
         assertEquals(100, equ1.getScore());
     }
 
+    /** On veut obtenir le pays de l'équipe */
     @Test
     public void testGetPays(){
         Pays france = new Pays("France");
@@ -36,18 +41,20 @@ public class TestEquipe {
         assertEquals(france, equ1.getPays());
     }
 
+    /** On veut pouvoir ajouter un athlète à l'équipe */
     @Test
     public void testAddAthletes(){
         Pays france = new Pays("France");
         Equipe equ1 = new Equipe("equ1", france);
-        equ1.addAthlete(new Athletes("Manaudou", "Florent", "male", 56, 87, 78, 34));
+        equ1.addAthlete(new Athlete("Manaudou", "Florent", "male", 56, 87, 78, 34, france));
     }
 
+    /** On veut obtenir les athlètes de l'équpe */
     @Test
     public void testGetAthletes(){
         Pays france = new Pays("France");
-        Athlete ath1 = new Athlete("Manaudou", "Florent", "male", 56, 87, 78, 34);
-        Athlete ath2 = new Athlete("Riner", "Teddy", "male", 89, 67, 53, 45);
+        Athlete ath1 = new Athlete("Manaudou", "Florent", "male", 56, 87, 78, 34, france);
+        Athlete ath2 = new Athlete("Riner", "Teddy", "male", 89, 67, 53, 45, france);
         Equipe equ1 = new Equipe("equ1", france);
         equ1.addAthlete(ath1);
         assertEquals(Arrays.asList(ath1, ath2), equ1.getAthletes());
