59e89a925c6efc7f5ec8bb7fd5467732f02db5b9
valentin <valentinhun@hotmail.fr>
2024-05-18 22:24:44 +0200

    ajout des méthodes ajouteEquipe et ajouteAthlete

--- src/jo/Pays.java
+++ src/jo/Pays.java
@@ -1,4 +1,5 @@
 package jo;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Pays {
@@ -12,7 +13,9 @@ public class Pays {
      * @param pays
      */
     public Pays(String pays) {
-
+        this.nomPays = pays;
+        this.equipes = new ArrayList<>();
+        this.athletes = new ArrayList<>();
     }
 
     /**
@@ -36,6 +39,22 @@ public class Pays {
      * @return
      */
     public List<Athlete> getAthletes() {
+
+    }
+    
+    /**
+     * Permet d'ajouter une équipe à la liste d'équipes du pays
+     * @param equipe
+     */
+    public void ajouteEquipe(Equipe equipe) {
+
+    }
+
+    /**
+     * Permet d'ajouter un athlète à la liste des athlètes
+     * @param athlete
+     */
+    public void ajouteAthlete(Athlete athlete) {
 
     }
 }
